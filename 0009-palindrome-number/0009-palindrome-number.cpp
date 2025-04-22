b8ad70b13263d8b67c3f2faa1356c9544fa594a6
srikanth-123178 <22951A6297@iare.ac.in>
2025-04-22 12:06:48 +0530

    Time: 4 ms (34.09%), Space: 8.6 MB (69.21%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        if (x < 0) return false;
+
+        int original = x;
+        long reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+
+        return reversed == original;
+    }
+};
