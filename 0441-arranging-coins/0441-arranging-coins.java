09ccd50b93d87193da5d01ef50a01682a304255e
srikanth-123178 <22951A6297@iare.ac.in>
2025-10-04 20:15:35 +0530

    Time: 8 ms (30.27%), Space: 41.4 MB (21.56%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count=0;
+        int i=1;
+        while (n>0){
+            n=n-i;
+            i++;
+            if (n>=0){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
